766b69d528fe5e7cfa54268af6d3a5c7082db0e8
Santiago <santipuentes2017@gmail.com>
2025-07-14 21:05:08 -0500

    Santiago

--- app/src/main/java/com/example/huertadirecta/CategoriasActivity.java
+++ app/src/main/java/com/example/huertadirecta/CategoriasActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -15,6 +16,7 @@ public class CategoriasActivity extends AppCompatActivity {
 
     Spinner spinner;
     Button btnVolver;
+    ImageView imagenCategoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class CategoriasActivity extends AppCompatActivity {
 
         spinner = findViewById(R.id.spinner);
         btnVolver = findViewById(R.id.button);
+        imagenCategoria = findViewById(R.id.imagenCategoria);
 
 
         String[] categorias = {"Selecciona una categoría", "Frutas", "Verduras", "Tubérculos", "Aromáticas"};
@@ -44,11 +47,52 @@ public class CategoriasActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {
                 // Nada seleccionado
             }
+
+
+        });
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String seleccion = parent.getItemAtPosition(position).toString();
+
+                switch (seleccion) {
+                    case "Frutas":
+                        imagenCategoria.setImageResource(R.drawable.frutas);
+                        imagenCategoria.setVisibility(View.VISIBLE);
+                        break;
+                    case "Verduras":
+                        imagenCategoria.setImageResource(R.drawable.verduras);
+                        imagenCategoria.setVisibility(View.VISIBLE);
+                        break;
+                    case "Tubérculos":
+                        imagenCategoria.setImageResource(R.drawable.tuberculos);
+                        imagenCategoria.setVisibility(View.VISIBLE);
+                        break;
+                    case "Aromáticas":
+                        imagenCategoria.setImageResource(R.drawable.aromaticas);
+                        imagenCategoria.setVisibility(View.VISIBLE);
+                        break;
+                    default:
+                        imagenCategoria.setVisibility(View.INVISIBLE);
+                        break;
+                }
+
+                if (!seleccion.equals("Selecciona una categoría")) {
+                    Toast.makeText(CategoriasActivity.this, "Seleccionaste: " + seleccion, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                imagenCategoria.setVisibility(View.INVISIBLE);
+            }
         });
 
         // Listener del botón VOLVER
         btnVolver.setOnClickListener(v -> {
-            // Finaliza esta actividad y regresa a la anterior
+            Intent intent = new Intent(CategoriasActivity.this, InicioActivity.class);
+            startActivity(intent);
             finish();
         });
     }
